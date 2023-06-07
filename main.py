42e9410e361e5ee104359fe7a246b79e5bb0da5e
14mthalvorson <14mthalvorson@gmail.com>
2023-06-07 18:18:18 -0400

    quick commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import yfinance as yf
 
 # Define the ticker symbol
@@ -10,4 +11,25 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='max')
 
 # See your data
-print(tickerDf)
+print(tickerDf.to_dict().keys())
+
+closes = []
+
+print(len(tickerDf.to_dict()['Close']))
+
+print(list(tickerDf.to_dict()['Close'].keys())[0])
+
+date_time = str(list(tickerDf.to_dict()['Close'].keys())[0])[:10]
+pattern = '%Y-%m-%d'
+epoch = int(time.mktime(time.strptime(date_time, pattern)))
+print(epoch)
+
+
+exit(0)
+
+for day in tickerDf.to_dict()['Close']:
+    closes.append((day[0], day[1]))
+
+print(closes[0])
+
+# closes.sort(key=lambda x: x[0])
