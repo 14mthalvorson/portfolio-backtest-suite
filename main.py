41f2c3e5b8391717f15c961ace2f53a9dfb75815
14mthalvorson <14mthalvorson@gmail.com>
2023-06-07 17:59:21 -0400

    quick commit

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import yfinance as yf
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Define the ticker symbol
+tickerSymbol = 'QQQ'
 
+# Get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Get the historical prices for this ticker
+tickerDf = tickerData.history(period='max')
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# See your data
+print(tickerDf)
