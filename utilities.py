8c65b17fcf2a17f64e2e6bb760343960778ed7b9
14mthalvorson <14mthalvorson@gmail.com>
2023-06-09 18:36:46 -0400

    can run multiple portfolios and auto name portfolios

--- utilities.py
+++ utilities.py
@@ -34,3 +34,50 @@ def lookup_price_history(cache, ticker):
         ticker_prices[year_decimal] = price
 
     cache[ticker] = ticker_prices
+
+
+def calculate_backtest_performance(cache, portfolio, settings):
+
+    rebalancing_frequency = settings.get('rebalancing_frequency', 0.25)
+
+    for ticker in portfolio.keys():
+        lookup_price_history(cache, ticker)
+
+    latest_start_date = 0
+
+    for ticker in portfolio.keys():
+        start_date = min(list(cache[ticker].keys()))
+        latest_start_date = max(latest_start_date, start_date)
+
+    print("Backtest Start Date:", latest_start_date)
+
+    if latest_start_date == 0:
+        print("Error with start dates")
+        exit(0)
+
+    # Portfolio Math
+    cumulative_return = 1
+    results = {latest_start_date: cumulative_return}
+    time = round(latest_start_date + rebalancing_frequency, 2)
+    today = 2023.4
+
+    while time <= today:
+        change = 0
+        for ticker in portfolio.keys():
+            a = get_price(cache[ticker], time - rebalancing_frequency)
+            b = get_price(cache[ticker], time)
+            if a is None or b is None:
+                print("None result", cache[ticker], time - rebalancing_frequency, time)
+                exit(0)
+            weight = portfolio[ticker] / 100
+            change += weight * (b / a - 1)
+
+        cumulative_return *= change + 1
+        results[time] = cumulative_return
+        time = round(time + rebalancing_frequency, 2)
+
+    # Analysis - Portfolio Metrics
+    cagr = cumulative_return ** (1 / (today - latest_start_date))
+    print('CAGR: {:.2%}'.format(cagr - 1))
+
+    return results
