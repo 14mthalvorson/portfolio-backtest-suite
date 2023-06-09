24a5880610083d58e11e599292c955f9b0d10ee6
14mthalvorson <14mthalvorson@gmail.com>
2023-06-09 18:29:38 -0400

    quick commit

--- main.py
+++ main.py
@@ -6,53 +6,55 @@ from utilities import *
 cache = {}
 
 # Create new portfolios
-portfolio = {'vfinx': 70, 'vustx': 30}
-
+portfolios = [{'vfinx': 70, 'vustx': 30}]
 rebalancing_frequency = 0.25
 
-for ticker in portfolio.keys():
-    lookup_price_history(cache, ticker)
+portfolio = portfolios[0]
 
-latest_start_date = 0
-
-for ticker in portfolio.keys():
-    start_date = min(list(cache[ticker].keys()))
-    latest_start_date = max(latest_start_date, start_date)
-
-print(latest_start_date)
-
-if latest_start_date == 0:
-    print("Error with start dates")
-    exit(0)
-
-
-# Portfolio Math
-cumulative_return = 1
-results = {latest_start_date: cumulative_return}
-time = round(latest_start_date + rebalancing_frequency, 2)
-today = 2023.4
-
-while time <= today:
-    change = 0
+def calculate_backtest_performance(portfolio):
     for ticker in portfolio.keys():
-        a = get_price(cache[ticker], time - rebalancing_frequency)
-        b = get_price(cache[ticker], time)
-        if a is None or b is None:
-            print("None result", cache[ticker], time - rebalancing_frequency, time)
-            exit(0)
-        weight = portfolio[ticker] / 100
-        change += weight * (b / a - 1)
+        lookup_price_history(cache, ticker)
 
-    cumulative_return *= change + 1
-    results[time] = cumulative_return
-    time = round(time + rebalancing_frequency, 2)
+    latest_start_date = 0
 
+    for ticker in portfolio.keys():
+        start_date = min(list(cache[ticker].keys()))
+        latest_start_date = max(latest_start_date, start_date)
 
-# Analysis - Portfolio Metrics
-cagr = cumulative_return ** (1 / (today - latest_start_date))
+    print(latest_start_date)
 
-print('CAGR: {:.2%}'.format(cagr - 1))
+    if latest_start_date == 0:
+        print("Error with start dates")
+        exit(0)
 
+    # Portfolio Math
+    cumulative_return = 1
+    results = {latest_start_date: cumulative_return}
+    time = round(latest_start_date + rebalancing_frequency, 2)
+    today = 2023.4
+
+    while time <= today:
+        change = 0
+        for ticker in portfolio.keys():
+            a = get_price(cache[ticker], time - rebalancing_frequency)
+            b = get_price(cache[ticker], time)
+            if a is None or b is None:
+                print("None result", cache[ticker], time - rebalancing_frequency, time)
+                exit(0)
+            weight = portfolio[ticker] / 100
+            change += weight * (b / a - 1)
+
+        cumulative_return *= change + 1
+        results[time] = cumulative_return
+        time = round(time + rebalancing_frequency, 2)
+
+    # Analysis - Portfolio Metrics
+    cagr = cumulative_return ** (1 / (today - latest_start_date))
+    print('CAGR: {:.2%}'.format(cagr - 1))
+
+    return results
+
+results = calculate_backtest_performance(portfolio)
 
 # Visualization
 x = list(results.keys())
