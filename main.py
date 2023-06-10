8e1b168e7444e5a3c32bde733e7dd7914439cef3
14mthalvorson <14mthalvorson@gmail.com>
2023-06-09 22:51:55 -0400

    quick commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ cache = {}
 # Create new portfolios
 portfolios = [
     {'tqqq': 32, 'cure': 21, 'lbay': 5, 'vpu': 4, 'vdc': 3, 'v': 1, 'amzn': 1.5, 'coin': 0.5, 'kmlm': 4.5, 'dbmf': 4.5, 'tmf': 4, 'edv': 4, 'btal': 14, 'vixm': 1},
-    {'tqqq': 32, 'cure': 21, 'vpu': 6, 'vdc': 6, 'v': 1, 'amzn': 2, 'fut': 4.5, 'wtmf': 4.5, 'tmf': 4, 'edv': 4, 'btal': 14, 'vixm': 1}]
+    {'tqqq': 32, 'cure': 21, 'vpu': 6, 'vdc': 6, 'v': 1, 'amzn': 2, 'wtmf': 9, 'tmf': 4, 'edv': 4, 'btal': 14, 'vixm': 1}]
 
 settings = {'rebalancing_frequency': 0.25}
 
@@ -31,7 +31,6 @@ plt.legend()
 
 # Display the plot
 plt.show()
-
 
 
 
